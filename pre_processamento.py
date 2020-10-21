cb89bde89dbb36457715a956da863416dc1f37a9
Pedro Galieta <pedrogalieta@hotmail.com>
2020-10-20 21:54:20 -0300

    Stereo calibration working

--- pre_processamento.py
+++ pre_processamento.py
@@ -21,7 +21,8 @@ def calibration(path):
     for file in glob.glob(path):
 
         img = cv2.imread(file)
-        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (1280, 720))   
         ret, corners = cv2.findChessboardCorners(image, (8,6), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
         if (ret == True): 
@@ -30,6 +31,10 @@ def calibration(path):
 
             refinedCorners = cv2.cornerSubPix(image, corners, (11,11), (-1, -1), criteria)
             imgPoints.append(refinedCorners)
+
+    print("Calib")
+    print(len(objPoints))
+    print(len(imgPoints))
 
     return(cv2.calibrateCamera(objPoints, imgPoints, image.shape[::-1], cameraMatrix=None, distCoeffs=None, flags=(cv2.CALIB_FIX_K3 + cv2.CALIB_ZERO_TANGENT_DIST)))
 
@@ -58,6 +63,7 @@ def generateUndistortedImagesFromVideo(mtx, dist, videoPath, cameraName):
         if ret == False:
             break
 
+        frame = cv2.resize(frame, (1280, 720))
         undistortedImage = undistort(mtx, dist, frame)
         cv2.imwrite(path + '/' + cameraName + 'Undistorted' + str(i) + '.jpg', undistortedImage)
         i+=1
@@ -104,6 +110,21 @@ def extractImagePoints():
         if cv2.waitKey(20) & 0xFF == 27: break
     cv2.destroyWindow('imageCam2')
 
+def stereoExtrinsic(mtx1, mtx2, dist1, dist2):
+
+    objectPoints = np.array([[0.0, 0.0, 0.0], [0.0, 1.4, 0.0], [2.6, 0.0, 0.0], [2.6, 1.4, 0.0]], dtype=np.float32)
+    lista = []
+    lista.append(objectPoints)
+    listaIm1 = []
+    listaIm1.append(np.array(imgPointsCam1, dtype=np.float32))
+    listaIm2 = []
+    listaIm2.append(np.array(imgPointsCam2, dtype=np.float32))
+    print("Stereo")
+    print(len(lista))
+    print(len(listaIm1))
+    print(len(listaIm2))
+    return cv2.stereoCalibrate(lista, listaIm1, listaIm2, mtx1, dist1, mtx2, dist2, (1280, 720), flags=cv2.CALIB_FIX_INTRINSIC)
+
 def imageClick(event, x, y, flags, param):
 
     #Check if left mouse button was clicked and stores RGB
@@ -111,11 +132,11 @@ def imageClick(event, x, y, flags, param):
 
         if param == 1:
 
-            imgPointsCam1.append((x, y))
+            imgPointsCam1.append([x, y])
 
         elif param == 2:
 
-            imgPointsCam2.append((x, y))
+            imgPointsCam2.append([x, y])
 
 if __name__ == "__main__":
 
@@ -129,15 +150,18 @@ if __name__ == "__main__":
     paths.append(input("\nCaminho para o dataset 2: ") + "/*.jpg")
 
     ret1, mtx1, dist1, rvecs1, tvecs1 = calibration(paths[0])
-    print(f"\ndist1: {dist1}")
+    print(f"\ndist1: {mtx1}")
     #generateUndistortedImagesFromVideo(mtx1, dist1, '/home/pedro/Documentos/UnB/PVC/StereoSystemOpencv/camera1.webm', 'camera1')
-    
+
     ret2, mtx2, dist2, rvecs2, tvecs2 = calibration(paths[1])
-    print(f"\ndist2: {dist2}")
+    print(f"\ndist2: {mtx2}")
     #generateUndistortedImagesFromVideo(mtx2, dist2, '/home/pedro/Documentos/UnB/PVC/StereoSystemOpencv/camera2.webm', 'camera2')
 
     extractImagePoints()
+    print(f"Image points 1: {imgPointsCam1}\nImage points 2: {imgPointsCam2}\n")
 
-    print(f"Image points 1: {imgPointsCam1}\nImage points 2: {imgPointsCam2}")
+    retVal, cm1, dc1, cm2, dc2, r, t, e, f = stereoExtrinsic(mtx1, mtx2, dist1, dist2)
 
+    print(f"Matrizes\nR: {r}\nT: {t}\nE: {e}\nF: {f}")
 
+    
